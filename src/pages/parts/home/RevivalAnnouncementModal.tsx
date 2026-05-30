16bf9a25a363eccdebb44255394eefe7bf12ae37
radhinmuhammed98 <radhinmuhammed98+1@gmail.com>
2026-05-30 17:24:42 +0530

    Clean up: Remove unused imports from RevivalAnnouncementModal

--- src/pages/parts/home/RevivalAnnouncementModal.tsx
+++ src/pages/parts/home/RevivalAnnouncementModal.tsx
@@ -1,14 +1,3 @@
-import { useCallback, useEffect } from "react";
-
-import { IconPatch } from "@/components/buttons/IconPatch";
-import { Icons } from "@/components/Icon";
-import { useModal } from "@/components/overlays/Modal";
-import { OverlayPortal } from "@/components/overlays/OverlayDisplay";
-import { Flare } from "@/components/utils/Flare";
-
-const MODAL_ID = "revival-announcement";
-const DISMISSED_KEY = `modal-${MODAL_ID}-dismissed`;
-
 export function RevivalAnnouncementModal() {
   // Popup disabled
   return null;
